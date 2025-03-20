191c1b4a6e312dae806a0bc9648be346ef5fe9cb
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-03-20 22:18:47 +0100

    Cors??

--- src/index.php
+++ src/index.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Handle preflight requests (OPTIONS method)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require "vendor/autoload.php";
 require "db.php";
 require "endpoints.php";
